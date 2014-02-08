51384fcd09852ed26eca881461a034c002e3b85c
Brian Moore <bmoore@book.com>
2014-02-08 16:39:39 -0500

    Add tweak-text func. Correct game-read func.

--- wizard.lisp
+++ wizard.lisp
@@ -65,11 +65,29 @@
     (cons 'items- (objects-at 'body *objects* *object-locations*)))
 
 (defun game-read ()
-    (let ((cmd (read-from-string
-                 (concatenate 'string "(" (read-line) ")"))))
+    (let ((cmd (read-from-string (concatenate 'string "(" (read-line) ")"))))
         (flet ((quote-it (x)
                  (list 'quote x)))
-          (cons (car cmd) (mapcar #'quote-it (cdr (cmd)))))))
+          (cons (car cmd) (mapcar #'quote-it (cdr cmd))))))
+
+(defun tweak-text (lst caps lit)
+    (when lst
+        (let ((item (car lst))
+              (rest (cdr lst)))
+
+            (cond ((eql item #\space) (cons item (tweak-text rest caps lit)))
+                  ((member item '(#\! #\? #\.)) (cons item (tweak-text rest t lit)))
+                  ((eql item #\") (tweak-text rest caps (not lit)))
+                  (lit (cons item (tweak-text rest nil lit)))
+                  (caps (cons (char-downcase item) (tweak-text rest nil lit)))
+                  (t (cons (char-downcase item) (tweak-text rest nil nil)))))))
+
+(defun game-print (lst)
+    (princ (coerce (tweak-text (coerce (string-trim "() " (prin1-to-string lst)) 'list)
+                               t
+                               nil)
+                    'string))
+    (fresh-line))
 
 (defun game-eval (sexp)
     (if (member (car sexp) *allowed-commands*)
@@ -81,4 +99,3 @@
         (unless (eq (car cmd) 'quit)
             (game-print (game-eval cmd))
             (game-repl))))
-
